7e414fc47683235960ce9de2d69484fbe90d77a7
Oğuz Kumcular <2012010217035@ogrenci.karabuk.edu.tr>
2017-07-26 07:43:56 +0300

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/omitv1.0/';
+$config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
